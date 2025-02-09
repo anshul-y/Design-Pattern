35b30c62818f577de3d279af02030f7390bdfc2d
Anshul Yadav <ayadav1@solveda.com>
2025-02-09 12:07:00 +0530

    Project config updated

---
+++ src/pattern/creational/builder/Vehicle.java
@@ -0,0 +1,49 @@
+package pattern.creational.builder;
+
+public class Vehicle {
+    //required parameter
+    private final String engine;
+    private final int wheel;
+
+    //optional parameter
+    private final int airbags;
+
+    private Vehicle(VehicleBuilder builder) {
+        this.engine = builder.engine;
+        this.wheel = builder.wheel;
+        this.airbags = builder.airbags;
+    }
+
+    public String getEngine() {
+        return this.engine;
+    }
+
+    public int getWheel() {
+        return this.wheel;
+    }
+
+    public int getAirbags() {
+        return this.airbags;
+    }
+
+    public static class VehicleBuilder {
+        private final String engine;
+        private final int wheel;
+
+        private int airbags;
+
+        public VehicleBuilder(String engine, int wheel) {
+            this.engine = engine;
+            this.wheel = wheel;
+        }
+
+        public VehicleBuilder setAirbags(int airbags) {
+            this.airbags = airbags;
+            return this;
+        }
+
+        public Vehicle build() {
+            return new Vehicle(this);
+        }
+    }
+}
